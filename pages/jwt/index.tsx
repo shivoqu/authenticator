2b61b1c828c524c802e9889d6b5af6cb076a72fe
Budiakov <Ivan.Budiakov@ontario.ca>
2022-12-02 21:07:44 -0500

    login-card wip to-do : color scheme

--- pages/jwt/index.tsx
+++ pages/jwt/index.tsx
@@ -1,35 +1,40 @@
 // import { getStaticProps } from "../users";
 import styles from "../../styles/LoginCard.module.css";
 import TextField from '@mui/material/TextField';
+import { Button } from "@mui/material";
 
 function Jwt() {
     return (
         <div tabIndex={0} className={styles.card}>
-            <h1>JWT LOGO HERE</h1>
-            <h2><span>JWT</span> Authentication</h2>
+            <h1><span>JWT</span> Authentication</h1>
 
-            <TextField 
-                name="jwt-user-name" 
-                id="jwt-user-name" 
-                label="Username" 
-                variant="outlined" 
-                />
+            <TextField
+                name="jwt-user-name"
+                id="jwt-user-name"
+                label="Username"
+                color="primary"
+                variant="outlined"
+                required
+            />
 
-            <TextField 
-                name="jwt-user-password" 
-                id="jwt-user-password" 
-                label="Password" 
-                variant="outlined" 
-                />    
+            <TextField
+                name="jwt-user-password"
+                id="jwt-user-password"
+                label="Password"
+                color="primary"
+                variant="outlined"
+                required
+            />
 
 
-
-            <p>
-                JSON Web Token is a proposed Internet standard for creating 
-                data with optional signature and/or optional encryption whose 
-                payload holds JSON that asserts some number of claims. The tokens 
-                are signed either using a private secret or a public/private key
-            </p>
+            <Button
+                name="login"
+                id="login"
+                variant="contained"
+                color="primary"
+                size="large"
+            >Login
+            </Button>
 
         </div>
 
