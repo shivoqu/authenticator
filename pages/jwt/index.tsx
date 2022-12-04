55798f087f09878d26b85c20ec301101f621c1d6
shivoqu <ivan.budiakov@student.sl.on.ca>
2022-12-03 23:56:27 -0500

    move from mui to tailwind

--- pages/jwt/index.tsx
+++ pages/jwt/index.tsx
@@ -1,41 +1,11 @@
 // import { getStaticProps } from "../users";
 import styles from "../../styles/LoginCard.module.css";
-import TextField from '@mui/material/TextField';
-import { Button } from "@mui/material";
 import UserForm from "../../lib/UserForm";
 
 function Jwt() {
     return (
         <div tabIndex={0} className={styles.card}>
             <h1><span>JWT</span> Authentication</h1>
-{/* 
-            <TextField
-                name="jwt-user-name"
-                id="jwt-user-name"
-                label="Username"
-                color="primary"
-                variant="outlined"
-                required
-            />
-
-            <TextField
-                name="jwt-user-password"
-                id="jwt-user-password"
-                label="Password"
-                color="primary"
-                variant="outlined"
-                required
-            />
-
-
-            <Button
-                name="login"
-                id="login"
-                variant="contained"
-                color="primary"
-                size="large"
-            >Login
-            </Button> */}
 
             <UserForm />
 
