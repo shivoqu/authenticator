2f68a99a5b80ef7db1d6b0189201754cd8727f98
shivoqu <ivan.budiakov@student.sl.on.ca>
2022-12-05 01:46:15 -0500

    restyle jwt login card with tailwind

--- lib/UserForm.tsx
+++ lib/UserForm.tsx
@@ -19,10 +19,50 @@ export default function UserForm() {
   };
 
   return (
-    <form className="bg-black" onSubmit={handleSubmit}>
-      <input type="text" name="username" />
-      <input type="password" name="password" />
-      <button type="submit">Login</button>
+    <form
+      className="mt-4 m-auto rounded shadow-md max-w-xs px-8 pt-6 pb-8 mb-4"
+      onSubmit={handleSubmit}
+    >
+      <div className="mb-4">
+        <label
+          className="block text-gray-700 text-sm font-bold mb-2"
+          htmlFor="username"
+        >
+          Username
+        </label>
+        <input
+          className="shadow rounded px-3 py-2 w-full text-gray-700 leading-tight  focus:outline-blue-500"
+          id="username"
+          type="text"
+          name="username"
+          placeholder="Username"
+          required
+        />
+      </div>
+
+      <div className="mb-6">
+        <label
+          className="block text-gray-700 text-sm font-bold mb-2"
+          htmlFor="password"
+        >
+          Password
+        </label>
+        <input
+          className="shadow appearance-none rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight  focus:outline-blue-500"
+          type="password"
+          name="password"
+          placeholder="**********"
+          required
+        />
+      </div>
+      <div className="flex w-full text-right">
+        <button
+          className="px-6 mr-0 ml-auto font-bold py-2 bg-blue-500 hover:bg-blue-700 text-white rounded outline-offset-0  focus:outline-blue-500"
+          type="submit"
+        >
+          Sign In
+        </button>
+      </div>
     </form>
   );
 }
--- pages/jwt/index.tsx
+++ pages/jwt/index.tsx
@@ -1,17 +1,19 @@
 // import { getStaticProps } from "../users";
-import styles from "../../styles/LoginCard.module.css";
 import UserForm from "../../lib/UserForm";
 
 function Jwt() {
-    return (
-        <div tabIndex={0} className={styles.card}>
-            <h1><span>JWT</span> Authentication</h1>
-
-            <UserForm />
-
-        </div>
-
-    )
+  return (
+    <div className="flex w-full h-full">
+      <div className="left w-1/2">
+        <h1 className="text-center font-bold text-3xl">JWT Authentication</h1>
+        <UserForm />
+      </div>
+      <div className="right w-1/2">
+        <p className="text-3xl font-bold text-center">right half</p>
+        <div className="mt-4 h-4/5 m-auto rounded shadow-md max-w-xs px-8 pt-6 pb-8 mb-4"></div>
+      </div>
+    </div>
+  );
 }
 
 export default Jwt;
