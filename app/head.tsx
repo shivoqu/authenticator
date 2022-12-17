f368920fbbaec079f00df4a343410c09d312ea33
Budiakov <ibudiakov@yahoo.com>
2022-12-16 22:58:47 -0500

    start the move to next13

---
+++ app/head.tsx
@@ -0,0 +1,15 @@
+export default function Head() {
+  return (
+    <>
+      <title>Authenticator</title>
+      <meta
+        name="description"  
+        content="A playground to explore different types of authentication/authorization
+            using different services in educational purposes."
+        key="description"
+      />
+        <link rel="icon" href="/favicon.ico" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,18 @@
+import Head from "next/head";
 import Footer from "../ui/Footer";
 import Header from "../ui/Header";
 
-
-export default function Layout({ children } : { children: React.ReactNode }) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <>
-      <Header />
-      <main>{children}</main>
-      <Footer />
-    </>
-  )
+    <html lang="en">
+      <head />
+      <body className="font-sans bg-gradient-to-b from-neutral-900 to-indigo-900">
+        <Header />
+        <main className="min-h-screen flex justify-center items-center flex-1 ">
+          {children}
+        </main>
+        <Footer />
+      </body>
+    </html>
+  );
 }
